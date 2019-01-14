cd336dbd87c40de4f29f4ff7d5bd0e07980464e6
Ted Hammarlund <TedHammarlund@gmail.com>
2019-01-14 15:45:37 +0100

    Stuff

---
+++ NOD Game Jam/Assets/ASR_ActSober/ASR_Scripts/ASR_RandomForce.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ASR_RandomForce : MonoBehaviour
+{
+    private Rigidbody[] _rigidbodies;
+    public float StartForce;
+    public float ForceMultiplier;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
