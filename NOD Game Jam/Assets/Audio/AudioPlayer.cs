5aea5f8ea491773bdbffb14c50dc370488cb4a60
wazzen <steven_keay@hotmail.com>
2019-01-14 15:47:31 +0100

    Audio Import

---
+++ NOD Game Jam/Assets/Audio/AudioPlayer.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour
+{
+    public static AudioPlayer Instance = null;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Play2DSound(CustomClip clip)
+    {
+        PlayClip(clip);
+    }
+
+    public void Play3DSound(CustomClip clip, GameObject sourceOfAudio)
+    {
+        PlayClip(clip, sourceOfAudio);
+    }
+
+    public void PlayClip(CustomClip clip, GameObject sourceOfAudio = null)
+    {
+        GameObject customSoundClip = new GameObject();
+        customSoundClip.name = "SFX(" + clip.name + ")";
+        customSoundClip.AddComponent<AudioSource>();
+
+        if (sourceOfAudio != null)
+        {
+            customSoundClip.transform.SetParent(sourceOfAudio.transform);
+            customSoundClip.transform.localPosition = new Vector3(0f, 0f, 0f);
+            customSoundClip.GetComponent<AudioSource>().spatialBlend = 1;
+        }
+        else
+        {
+            customSoundClip.GetComponent<AudioSource>().spatialBlend = 0;
+        }
+        clip.PlayCustomClip(customSoundClip.GetComponent<AudioSource>());
+        if (!clip.Loop)
+        {
+            Instance.StartCoroutine(WaitAndDestroy(customSoundClip.GetComponent<AudioSource>()));
+        }
+    }
+
+    private IEnumerator WaitAndDestroy(AudioSource source)
+    {
+        while (source.isPlaying)
+        {
+            yield return 0;
+        }
+        Destroy(source.gameObject);
+        yield return 0;
+    }
+}
---
+++ NOD Game Jam/Assets/Audio/SoundManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance = null;
+    [Header("Custom Clips")]
+    public CustomClip TableMelting;
+    public CustomClip LavaSFX;
+    public CustomClip CupStack;
+
+    private void Awake()
+    {
+       if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlayTableMelting(GameObject sourceOfSound)
+    {
+        AudioPlayer.Instance.Play3DSound(TableMelting, sourceOfSound);
+    }
+
+    public void PlayLavaLoop()
+    {
+        AudioPlayer.Instance.Play2DSound(LavaSFX);
+    }
+    public void PlayStackCup()
+    {
+        AudioPlayer.Instance.Play2DSound(CupStack);
+    }
+
+}
