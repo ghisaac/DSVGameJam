18e2d1cc91caada578d786bebc742ed837cf6b54
marinlord <morrismertner@gmail.com>
2019-01-14 15:09:20 +0100

    Hubmanager
    Länge leve huben

--- NOD Game Jam/Assets/General/Game Loop/Scripts/GameManager.cs
+++ NOD Game Jam/Assets/General/Game Loop/Scripts/GameManager.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private static GameManager _instance;
+    public static GameManager Instance
     {
-        
+        get
+        {
+            return _instance;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void Awake()
     {
-        
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        DontDestroyOnLoad(this.gameObject);
+        _instance = this;
     }
-}
+
+ }
---
+++ NOD Game Jam/Assets/General/Hub/Scripts/HubManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using Rewired;
+using UnityEngine;
+
+public class HubManager : MonoBehaviour
+{
+
+    public GameObject[] levels;
+    public int current;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        current = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+
+    }
+}
