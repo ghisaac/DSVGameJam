14678751f01f77c21f73a48c5d74f7c123c0cb12
Ted Hammarlund <TedHammarlund@gmail.com>
2019-01-14 15:32:03 +0100

    Scripts and scene

---
+++ NOD Game Jam/Assets/ASR_ActSober/ASR_Scripts/ASR_CharacterController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ASR_CharacterController : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    private float _horizontal;
+    public float Force = 20f;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        _horizontal = Input.GetAxisRaw("Horizontal");
+
+        Vector3 direction = -transform.right * _horizontal;
+
+        _rigidbody.AddForce(direction * Force, ForceMode.Acceleration);
+
+    }
+}
