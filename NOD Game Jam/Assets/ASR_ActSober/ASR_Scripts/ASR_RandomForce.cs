75ea3cda1a1d3b7762554a7940827a5ddb844319
Ted Hammarlund <TedHammarlund@gmail.com>
2019-01-14 16:30:34 +0100

    playerprefabs

--- NOD Game Jam/Assets/ASR_ActSober/ASR_Scripts/ASR_RandomForce.cs
+++ NOD Game Jam/Assets/ASR_ActSober/ASR_Scripts/ASR_RandomForce.cs
@@ -4,18 +4,43 @@ using UnityEngine;
 
 public class ASR_RandomForce : MonoBehaviour
 {
-    private Rigidbody[] _rigidbodies;
+    public Rigidbody[] _rigidbodies;
     public float StartForce;
     public float ForceMultiplier;
-    // Start is called before the first frame update
+    private float _force;
+    [Header("Random Force MinMax time")]
+    public float MinRandomTime;
+    public float MaxRandomTime;
+    
     void Start()
+    {
+        _force = StartForce;
+        StartCoroutine(RandomForceTimer());
+    }
+
+    void Update()
     {
 
     }
 
-    // Update is called once per frame
-    void Update()
+    private void AddRandomForce()
     {
+        float randomHorizontal = Random.value > 0.5 ? 1 : -1;
 
+
+
+        foreach (Rigidbody rb in _rigidbodies)
+        {
+
+            rb.AddForce(rb.transform.right * randomHorizontal * _force, ForceMode.Impulse);
+        }
+        _force *= ForceMultiplier;
+    }
+
+    private IEnumerator RandomForceTimer()
+    {
+        yield return new WaitForSeconds(Random.Range(MinRandomTime, MaxRandomTime));
+        AddRandomForce();
+        StartCoroutine(RandomForceTimer());
     }
 }
