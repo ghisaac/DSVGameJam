77c6017c7ec12cb1707c56a692b4e88cf2e7904a
NiklasLand <NiklasTorbjornlandberg@gmail.com>
2019-01-14 15:58:47 +0100

    Update PlayerBaseState.cs

--- NOD Game Jam/Assets/General/Player/PlayerController/StateMachine/PlayerBaseState.cs
+++ NOD Game Jam/Assets/General/Player/PlayerController/StateMachine/PlayerBaseState.cs
@@ -6,7 +6,7 @@ public abstract class PlayerBaseState : State
 {
     protected PlayerController controller { get { return (PlayerController)StateMachine.owner; } }
     protected Transform transform { get { return controller.transform; } }
-    protected Vector2 Velocity { get { return controller.Velocity; } set { controller.Velocity = value; } }
+    protected Vector3 Velocity { get { return controller.Velocity; } set { controller.Velocity = value; } }
     protected Rewired.Player RewierdPlayer { get { return Rewired.ReInput.players.GetPlayer(controller.myPlayer.RewierdId); } }
 
     //Protected methods
