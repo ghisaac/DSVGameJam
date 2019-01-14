f82fdea4ac486e339e6ec249bef4b6652f568643
marinlord <morrismertner@gmail.com>
2019-01-14 16:56:34 +0100

    Mycket tempsaker för testning

---
+++ NOD Game Jam/Assets/General/Hub/Scripts/LevelPinSceneReference.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelPinSceneReference : MonoBehaviour
+{
+    public Scene myScene;
+    public int mySceneIndex;
+
+    public Scene GetMyScene()
+    {
+        return SceneManager.GetSceneByBuildIndex(mySceneIndex);
+    }
+}
