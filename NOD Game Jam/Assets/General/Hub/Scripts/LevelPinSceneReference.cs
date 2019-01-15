a74c8793edae076c2c7fd1449ec7f72685f108d5
ghisaac <ghisaac@outlook.com>
2019-01-15 10:20:01 +0100

    Fixing loading scenes in HUB

--- NOD Game Jam/Assets/General/Hub/Scripts/LevelPinSceneReference.cs
+++ NOD Game Jam/Assets/General/Hub/Scripts/LevelPinSceneReference.cs
@@ -6,13 +6,7 @@ using UnityEngine;
 public class LevelPinSceneReference : MonoBehaviour
 {
     [SerializeField]
-    private Scene pinScene;
     private int pinSceneIndex;
-
-    private void Start()
-    {
-        pinSceneIndex = pinScene.buildIndex;
-    }
 
     public int GetSceneIndex()
     {
