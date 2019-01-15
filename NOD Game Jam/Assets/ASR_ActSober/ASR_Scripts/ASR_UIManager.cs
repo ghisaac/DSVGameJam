0413f04022c1b819732a6b0106589fe21462f22f
Ted Hammarlund <TedHammarlund@gmail.com>
2019-01-15 16:34:53 +0100

    stuff

---
+++ NOD Game Jam/Assets/ASR_ActSober/ASR_Scripts/ASR_UIManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ASR_UIManager : MonoBehaviour
+{
+    public TextMeshProUGUI RoundNumber, RoundCountdown, Winscreen;
+    public int TestWinNumber;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*
+        RoundNumber.enabled = false;
+        RoundCountdown.enabled = false;
+        Winscreen.enabled = false;
+        */
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            ActivateWinScreen(TestWinNumber);
+        }
+    }
+
+    public void ActivateWinScreen(int playerWinNumber)
+    {
+        Winscreen.SetText("Player {0} Wins!", playerWinNumber);
+        StartCoroutine(WinScreenCoroutine());
+    }
+
+    private IEnumerator WinScreenCoroutine()
+    {
+        Debug.Log("Entered winscreen activate");
+        Winscreen.gameObject.SetActive(true);
+        
+        yield return new WaitForSeconds(3f);
+
+        Winscreen.gameObject.SetActive(false);
+    }
+}
