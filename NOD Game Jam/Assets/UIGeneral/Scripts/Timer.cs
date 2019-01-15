13ca9a206e93d8b6e01a79390fa3c763f2d5d5f1
todorowski <emma.t.wahrenberg@gmail.com>
2019-01-15 14:44:09 +0100

    Timer

---
+++ NOD Game Jam/Assets/UIGeneral/Scripts/Timer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class Timer : MonoBehaviour
+{
+    private static float DefaultLenght = 60f*5f;
+    public static float TimerValue;
+    private bool running;
+    private static Timer instance;
+
+    public static TextMeshProUGUI timer;
+
+    public void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+        StartTimer();
+            
+    }
+
+    private void Update()
+    {
+        TimerValue -= Time.deltaTime;
+        timer.text = TimerValue.ToString("00.00");
+    }
+
+    public static void StartTimer(float Length = -1)
+    {
+        instance.running = true;
+        TimerValue = Length > -1 ? Length : DefaultLenght;
+    }
+
+    public static void Hide()
+    {
+        timer.enabled = false;
+    }
+
+    public static void Pause()
+    {
+        instance.running = false;
+    }
+
+}
