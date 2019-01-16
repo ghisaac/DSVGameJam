02a53e64b9684cba2f0170e3948d5caa79e876c2
Albin <38533964+Aulbn@users.noreply.github.com>
2019-01-16 15:43:30 +0100

    PlayerPrefabs
    +Animation Controller
    +4 prefabs
    +Animation-script

---
+++ NOD Game Jam/Assets/KSR/Scripts/KSR_PlayerAnimation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KSR_PlayerAnimation : MonoBehaviour
+{
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<PlayerController>().animator;    
+    }
+
+    void Update()
+    {
+        
+    }
+}
