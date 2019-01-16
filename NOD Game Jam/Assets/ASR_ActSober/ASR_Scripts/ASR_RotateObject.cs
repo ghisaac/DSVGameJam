71d28e324f723c381f2114cbf10b13f710e65f65
vii9085 <vili9085@student.su.se>
2019-01-16 16:46:34 +0100

    sfdsf

---
+++ NOD Game Jam/Assets/ASR_ActSober/ASR_Scripts/ASR_RotateObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ASR_RotateObject : MonoBehaviour
+{
+    public float RotateY = 15f;
+    public float RotateX = 15f;
+    public float RotateZ = 15f;
+
+    void Update () {
+        transform.Rotate(Vector3.up, RotateY * Time.deltaTime);
+        transform.Rotate(Vector3.right, (RotateX  * Time.deltaTime));
+        transform.Rotate(Vector3.forward, (RotateZ * Time.deltaTime));
+
+    }
+}
