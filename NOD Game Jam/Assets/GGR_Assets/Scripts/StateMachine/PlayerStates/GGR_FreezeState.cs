35a2a7a73bf9af65e7a14e1b91f95da92a4e1ba7
NiklasLand <NiklasTorbjornlandberg@gmail.com>
2019-01-16 15:10:25 +0100

    Revert "Merge branch 'master' of https://github.com/ghisaac/NOD-Game-Jam"
    This reverts commit 63a2d46fdb3de167d9b8ef5091a40384500d7a6b, reversing
    changes made to 343aa37cbe4c3fd15e0d01279005fd3569bac6d0.

--- NOD Game Jam/Assets/GGR_Assets/Scripts/StateMachine/PlayerStates/GGR_FreezeState.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-[CreateAssetMenu(menuName = "GGR/Player/State/PlayerFreezeState")]
-public class GGR_FreezeState : PlayerBaseState
-{
-
-}
