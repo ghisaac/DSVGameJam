cce81ce8c5c820a6082372cd67e9479251c45221
Daniel Åkesson <argilstudio@gmail.com>
2019-01-17 13:35:20 +0100

    Fixade leave i hub grafics

---
+++ NOD Game Jam/Assets/General/SplashScreen.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour
+{
+
+    public GameObject FLickerGuy;
+    public float FlickerTime;
+    private float currentTime;
+    void Update()
+    {
+        FlickerGuy();
+        CheckIfAnyPlayerPressA();
+    }
+
+    private void FlickerGuy()
+    {
+        currentTime += Time.deltaTime;
+        if(currentTime > FlickerTime)
+        {
+            currentTime = 0f;
+            FLickerGuy.SetActive(!FLickerGuy.activeSelf);
+        }
+    }
+
+    private void CheckIfAnyPlayerPressA()
+    {
+        //Check if any player presses a button
+        for (int i = 0; i < ReInput.players.AllPlayers.Count; i++)
+        {
+            if (ReInput.players.AllPlayers[i].GetButtonDown("A"))
+                LoadHub();
+        }
+    }
+    private void LoadHub()
+    {
+        SceneManager.LoadScene("Hub");
+    }
+}
