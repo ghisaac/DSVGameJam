3eb923005780f7c19392b9fd98eea06a87a7ecc1
ghisaac <ghisaac@outlook.com>
2019-01-17 15:47:42 +0100

    byebye

--- NOD Game Jam/Assets/HHS_Assets/HHS_Scripts/HHS_FrozenState.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Player/State/HHS_States/HHS_PlayerFrozenState")]
-public class HHS_FrozenState : PlayerBaseState
-{
-
-}
