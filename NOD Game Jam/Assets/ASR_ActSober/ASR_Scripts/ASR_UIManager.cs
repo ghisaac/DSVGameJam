da1044c95636a2406af05ab1ae01305204251273
Ted Hammarlund <TedHammarlund@gmail.com>
2019-01-17 13:24:44 +0100

    code comments

--- NOD Game Jam/Assets/ASR_ActSober/ASR_Scripts/ASR_UIManager.cs
+++ NOD Game Jam/Assets/ASR_ActSober/ASR_Scripts/ASR_UIManager.cs
@@ -10,13 +10,15 @@ public class ASR_UIManager : MonoBehaviour
     public TextMeshProUGUI[] PlayerPlacementGuis;
     public int TestWinNumber;
 
+    //Dictionaries to hold the score and placement UI for each active player
     private Dictionary<ASR_CharacterController, TextMeshProUGUI> _scoreGUIDictionary = new Dictionary<ASR_CharacterController, TextMeshProUGUI>();
     private Dictionary<ASR_CharacterController, TextMeshProUGUI> _placementGUIDictionary = new Dictionary<ASR_CharacterController, TextMeshProUGUI>();
 
     public GameObject[] Portraits;
 
     public GameObject ConfettiParticles;
-
+    
+    //Method to fill up the dictionaries with the active players in the scene, also activates each active players score UI
     public void FillUpDictionarys(ASR_CharacterController[] characterControllerList)
     {
         for(int i = 0; i < characterControllerList.Length; i++)
@@ -28,11 +30,13 @@ public class ASR_UIManager : MonoBehaviour
         }
     }
 
+    //Method to set players score in the UI
     public void SetScoreGui(ASR_CharacterController characterController, int score)
     {
         _scoreGUIDictionary[characterController].SetText("Score: {0}", score);
     }
-
+    
+    //Method to set players placement in the UI, also activates the placement UI
     public void SetPlacementGui(ASR_CharacterController characterController, int placement)
     {
         switch (placement)
@@ -56,27 +60,32 @@ public class ASR_UIManager : MonoBehaviour
         //_placementGUIDictionary[characterController].gameObject.SetActive(true);
     }
 
+    //Method to set a players portrait and score active in the UI
     public void SetScoreGuiActive(ASR_CharacterController characterController)
     {
         Portraits[characterController.Player.RewierdId].SetActive(true);
         _scoreGUIDictionary[characterController].gameObject.SetActive(true);
     }
 
+    //Method to set a players score UI inactive
     public void SetScoreGuiInActive(ASR_CharacterController characterController)
     {
         _scoreGUIDictionary[characterController].gameObject.SetActive(false);
     }
 
+    //Method to set a players placement UI Active
     public void SetPlacementGUIActive(ASR_CharacterController characterController)
     {
         _placementGUIDictionary[characterController].gameObject.SetActive(true);
     }
 
+    //Method to set a players placement UI Inactive
     public void SetPlacementGUIInActive(ASR_CharacterController characterController)
     {
         _placementGUIDictionary[characterController].gameObject.SetActive(false);
     }
 
+    //Method to inactivate all placement UI:s active on screen
     public void InactivatePlacementGui()
     {
         for(int i = 0; i < PlayerPlacementGuis.Length; i++)
@@ -85,17 +94,19 @@ public class ASR_UIManager : MonoBehaviour
         }
     }
 
-
+    //Method to get a players score UI
     public TextMeshProUGUI GetScoreGUI(ASR_CharacterController characterController)
     {
         return _scoreGUIDictionary[characterController];
     }
 
+    //Method to get a players placement UI
     public TextMeshProUGUI GetPlacementGUI(ASR_CharacterController characterController)
     {
         return _placementGUIDictionary[characterController];
     }
 
+    //Coroutine to activate the round UI and countdown UI and start the countdown
     public IEnumerator RoundCountdownTimer(int roundNumber)
     {
         RoundNumber.SetText("Round {0}", roundNumber);
@@ -114,6 +125,7 @@ public class ASR_UIManager : MonoBehaviour
 
     }
 
+    //Coroutine to activate the winscreen when all rounds are completed
     public IEnumerator WinScreenCoroutine(string playerName)
     {
         SoundManager.Instance.PlayVictorySound();
