f7ecb7aa7ee2d0f3d867cbaaee6eca4beb43403c
carkhem <38533272+carkhem@users.noreply.github.com>
2019-01-17 10:27:43 +0100

    ParticleSpawner

---
+++ NOD Game Jam/Assets/KSR/KSR_Scripts/KSR_ParticleSpawner.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KSR_ParticleSpawner : MonoBehaviour
+{
+    [Header("DrivingParticles")]
+    public GameObject smokeTrailParticle;
+    public GameObject brakeParticle;
+    [Header("EnviromentParticles")]
+    public GameObject dirtParticle;
+    public GameObject leafParticle;
+    [Header("ImpactParticles")]
+    public GameObject bigImpactParticle;
+    public GameObject smallImpactParticle;
+
+    [HideInInspector]
+    public KSR_ParticleSpawner instance;
+
+    public void Awake()
+    {
+        instance = this;
+    }
+
+    public void SpawnSmokeTrailParticle(Vector3 pos, Quaternion rotation)
+    {
+        GameObject temp = smokeTrailParticle;
+        Instantiate(temp, pos, rotation);
+    }
+
+    public void SpawnBrakeParticle(Vector3 pos, Quaternion rotation)
+    {
+        GameObject temp = brakeParticle;
+        Instantiate(temp, pos, rotation);
+    }
+
+    public void SpawnDirtParticles(Vector3 pos, Quaternion rotation)
+    {
+        GameObject temp = dirtParticle;
+        Instantiate(temp, pos, rotation);
+    }
+
+    public void SpawnLeafPatricles(Vector3 pos, Quaternion rotation)
+    {
+        GameObject temp = leafParticle;
+        Instantiate(temp, pos, rotation);
+    }
+
+    public void SpawnBigImpactParticle(Vector3 pos, Quaternion rotation)
+    {
+        GameObject temp = bigImpactParticle;
+        Instantiate(temp, pos, rotation);
+    }
+
+    public void SpawnSmallImpact(Vector3 pos, Quaternion rotation)
+    {
+        GameObject temp = smallImpactParticle;
+        Instantiate(temp, pos, rotation);
+    }
+
+   
+}
